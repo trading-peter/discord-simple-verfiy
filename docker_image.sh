954df19b5453cd5d20c679b09602efe8f0f68e3f
pk <pk@codeblob.work>
2022-11-11 22:56:37 +0100

    Fix name

--- docker_image.sh
+++ docker_image.sh
@@ -13,6 +13,6 @@ CGO_ENABLED=0 go build -ldflags="-s -w" -o ../bot .
 
 # A second run is needed to build the final image.
 cd ..
-docker build -f sources/Dockerfile --no-cache -t simple-verify-bot:${ver} .
-docker save simple_verify_bot:${ver} > docker_out/simple-verify-bot_${ver}.tar
+docker build -f sources/Dockerfile --no-cache -t simple_verify_bot:${ver} .
+docker save simple_verify_bot:${ver} > docker_out/simple_verify_bot_${ver}.tar
 rm -rf sources bot
